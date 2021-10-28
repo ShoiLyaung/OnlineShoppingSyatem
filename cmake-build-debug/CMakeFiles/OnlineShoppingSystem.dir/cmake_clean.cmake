c0c4a2620dd7cb550a57ec052d4b4fff1329cfa7
ShoiLyaung <2596985510@qq.com>
2021-10-28 15:48:49 +0800

    创建项目

---
+++ cmake-build-debug/CMakeFiles/OnlineShoppingSystem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OnlineShoppingSystem.dir/main.cpp.obj"
+  "OnlineShoppingSystem.exe"
+  "OnlineShoppingSystem.exe.manifest"
+  "OnlineShoppingSystem.pdb"
+  "libOnlineShoppingSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OnlineShoppingSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OnlineShoppingSystem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OnlineShoppingSystem.
+# This may be replaced when dependencies are built.
